c8c00f8e7d9e441af976700cf9b32b558fd30b88
supp7350 <pradnyapatil551@gmail.com>
2025-07-18 01:14:36 +0530

    Add files via upload

---
+++ DayTwo/day_two/Conditional_Statements/IfDemo.java
@@ -0,0 +1,23 @@
+package DayTwo.day_two.Conditional_Statements;
+
+public class IfDemo {
+	
+	public static void main(String[] args)
+	{
+		int x=7;
+		
+		if(x==5)
+		{
+			System.out.println("Value of x is 5");
+		}
+		
+		int age = 17;
+		
+		if(age > 18)
+		{
+			System.out.println("You are eligible for voting");
+		}
+		
+	}
+
+}
